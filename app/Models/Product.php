faae5efe75120af95095b6aee0719ef2e2eff066
Asep Suprapto <asepsuprapto.id@gmail.com>
2025-12-26 14:45:29 +0700

    add: fillable product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'product_category_id',
+        'code',
+        'name',
+        'unit_measurement_id',
+        'sales_price',
+        'purchase_price',
+        'sales_tax_id',
+        'purchase_tax_id',
+        'minimum_stock',
+        'description',
+        'image',
+        'is_active',
+    ];
 }
