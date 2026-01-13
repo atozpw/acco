91966a31bed3f4ce60b50971cb578e1c7dac6593
Asep Suprapto <asepsuprapto.id@gmail.com>
2026-01-13 11:12:20 +0700

    feat: cash advance form request

---
+++ app/Http/Requests/CashBank/StoreCashAdvanceExpenseRequest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Requests\CashBank;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCashAdvanceExpenseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'contact_id' => [
+                'required',
+                'integer',
+                'exists:contacts,id',
+            ],
+
+            'coa_id' => [
+                'required',
+                'integer',
+                'exists:coas,id',
+            ],
+
+            'reference_no' => [
+                'required',
+                'string',
+                'max:10',
+                'unique:cash_advance_expenses,reference_no',
+            ],
+
+            'date' => [
+                'required',
+                'date_format:Y-m-d',
+            ],
+
+            'description' => [
+                'required',
+                'string',
+                'max:100',
+            ],
+
+            'amount' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'tax_amount' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'total' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'details.*.cash_advance_classification_id' => [
+                'required',
+                'integer',
+                'exists:cash_advance_classifications,id',
+            ],
+
+            'details.*.amount' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'details.*.tax_amount' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'details.*.total' => [
+                'required',
+                'decimal:2',
+            ],
+
+            'details.*.note' => [
+                'nullable',
+                'string',
+                'max:100',
+            ],
+
+            'details.*.tax_id' => [
+                'nullable',
+                'integer',
+                'exists:taxes,id',
+            ],
+
+            'details.*.department_id' => [
+                'required',
+                'integer',
+                'exists:departments,id',
+            ],
+
+            'details.*.project_id' => [
+                'nullable',
+                'integer',
+                'exists:projects,id',
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/Master/StoreCashAdvanceClassificationRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Master;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCashAdvanceClassificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:50',
+            ],
+
+            'cash_advance_income_coa_id' => [
+                'nullable',
+                'integer',
+                'exists:coas,id',
+            ],
+
+            'cash_advance_expense_coa_id' => [
+                'nullable',
+                'integer',
+                'exists:coas,id',
+            ],
+
+            'is_active' => [
+                'required',
+                'boolean',
+            ],
+        ];
+    }
+}
