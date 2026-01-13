0bd3755b19151c09bd3f5862702d570b6834a0e0
Asep Suprapto <asepsuprapto.id@gmail.com>
2026-01-13 15:44:29 +0700

    feat: payable and receivable form request

---
+++ app/Http/Requests/BeginningBalance/StorePayableRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests\BeginningBalance;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePayableRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'contact_id' => [
+                'required',
+                'integer',
+                'exists:contacts,id',
+            ],
+
+            'coa_id' => [
+                'required',
+                'integer',
+                'exists:coas,id',
+            ],
+
+            'reference_no' => [
+                'required',
+                'string',
+                'max:10',
+                'unique:purchase_invoices,reference_no',
+            ],
+
+            'date' => [
+                'required',
+                'date_format:Y-m-d',
+            ],
+
+            'amount' => [
+                'required',
+                'decimal:2',
+            ],
+        ];
+    }
+}
