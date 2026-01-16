bb410f86a6b4126fc9375fe795b5e4d2eb94381c
Asep Suprapto <asepsuprapto.id@gmail.com>
2026-01-16 22:01:48 +0700

    feat: contact has many sales invoice and purchase invoice

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
@@ -28,5 +29,15 @@ class Contact extends Model
     protected function active(Builder $query): void
     {
         $query->where('is_active', 1);
+    }
+
+    public function salesInvoices(): HasMany
+    {
+        return $this->hasMany(SalesInvoice::class);
+    }
+
+    public function purchasesInvoices(): HasMany
+    {
+        return $this->hasMany(PurchaseInvoice::class);
     }
 }
